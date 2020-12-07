42e59e6b8687ab3e2cc3fa41256045f5e0d5a35e
mental-block <aaron.tibben@gmail.com>
2020-12-07 18:21:45 -0500

    Add precompiled assets for Heroku

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'dist')));
 
-  // app.get("/*", (req, res) => {
-  //   res.sendFile(
-  //     path.join(__dirname, "../", "frontend", "dist", "index.html")
-  //   );
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../", "frontend", "dist", "index.html")
+    );
+  });
 } else {
   app.use(express.static('./public'));
 }
