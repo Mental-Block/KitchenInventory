587806e48e5c4e04ff134f68e3414cd78e6cb764
mental-block <aaron.tibben@gmail.com>
2020-12-06 20:21:00 -0500

    fixed webpack issues

---
+++ frontend/webpack.common.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index",
+    resolve: {
+        alias: {
+            root: path.resolve(__dirname, "./src"),
+        },
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve("./public"),
+        proxy: {
+            "/api/**": {
+                target: "http://localhost:5000/",
+                pathRewrite: { "^/api": "" },
+                secure: false,
+                changeOrigin: true,
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: ["html-loader"],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: ["url-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
