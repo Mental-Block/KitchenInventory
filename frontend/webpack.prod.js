a0c84a3707fe1aad628f640d84a68786e5dc5933
mental-block <aaron.tibben@gmail.com>
2020-12-07 20:25:36 -0500

    push to heroku

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -4,10 +4,6 @@ const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
     mode: "production",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
     devServer: {
         contentBase: path.resolve("./dist"),
         proxy: {
