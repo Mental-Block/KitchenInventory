62951562e589226be7dc14bb795133b591558ada
mental-block <aaron.tibben@gmail.com>
2020-12-07 19:27:20 -0500

    Add precompiled assets for Heroku

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(express.static(path.join(__dirname, "../", "frontend", "dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "../", "frontend", "dist", "index.html")
     );
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -14,7 +14,7 @@ module.exports = merge(common, {
             "/api/**": {
                 target: "https://kitchen--inventory.herokuapp.com/",
                 pathRewrite: { "^/api": "" },
-                secure: false,
+                secure: true,
                 changeOrigin: true,
             },
         },
