b4d541387847cbf05e70c14ea9ea14830dcd68b0
mental-block <aaron.tibben@gmail.com>
2020-12-07 20:27:00 -0500

    push to heroku

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -4,4 +4,19 @@ const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
     mode: "production",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        contentBase: path.resolve("./dist"),
+        proxy: {
+            "/api/**": {
+                target: "https://kitchen--inventory.herokuapp.com/",
+                pathRewrite: { "^/api": "" },
+                secure: true,
+                changeOrigin: true,
+            },
+        },
+    }
 });
