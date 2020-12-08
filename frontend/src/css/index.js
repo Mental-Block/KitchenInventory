7e63230e61fa7b61045d79e87a75256be224eb44
mental-block <aaron.tibben@gmail.com>
2020-12-08 15:41:49 -0500

    deploy

--- frontend/src/css/index.js
+++ frontend/src/css/index.js
@@ -1,4 +1,4 @@
-export { default as GlobalStyle } from "./GlobalStyle";
+export { GlobalStyle } from "./GlobalStyle";
 export { default as DefaultTheme } from "./Theme";
 export { StyledGreenButton, StyledRedButton } from "./button";
 export { StyledWhiteHeader, StyledWhiteSubHeader, StyledHeader } from "./header";
