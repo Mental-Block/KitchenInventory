96621e8e509687cc0455e49fa84ef1b668b8351f
mental-block <aaron.tibben@gmail.com>
2020-12-08 16:19:25 -0500

    deploy

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader!css-loader", "css-loader"],
+                use: ["style-loader", "css-loader"],
                 sideEffects: true
             },
             {
