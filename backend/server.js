9a925681ba0873e53fee8cbff84208e8d1df9b44
mental-block <aaron.tibben@gmail.com>
2020-12-09 07:00:09 -0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '/dist')));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend/", "dist")
+      path.join(__dirname, "../", "frontend", "dist", "index.html")
     );
   });
 } else {
