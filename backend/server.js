4ad9a299d5ec4c50c27891465b6c9134183ff3c9
mental-block <aaron.tibben@gmail.com>
2020-12-13 14:10:19 -0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path")
 const fs = require("fs");
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 require("dotenv").config();
 
 const app = express();
-
-app.use(
-  '/api/**',
-  createProxyMiddleware({
-    target: "http://localhost:5000",
-    "secure": false,
-    "changeOrigin": true,
-    pathRewrite: { "^/api/": "/api/" },
-  })
-);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,7 +15,6 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 if (process.env.NODE_ENV === "production") {
-
   app.use(express.static(path.join(__dirname, "../", "frontend/", "dist")));
   app.get("/*", (req, res) => {
     res.sendFile(
